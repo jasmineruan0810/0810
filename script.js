ace54e1d08be699a2e26b6177adadcb25a213717
jasmineruan0810 <jasmineruan0810@gmail.com>
2026-04-27 15:46:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.querySelector(".close-btn");
+const viewButtons = document.querySelectorAll(".view-project-btn");
+
+viewButtons.forEach(button => {
+  button.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    const imageSrc = this.getAttribute("data-image"); 
+    modalImg.src = imageSrc; 
+    modal.style.display = "flex"; 
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
